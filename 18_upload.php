1b7c87d90dd4684e2fe723982031ff56d390f5fd
kunwarvk01 <kunwarvk01@gmail.com>
2023-10-17 12:27:51 +0530

    changes

--- 18_upload.php
+++ 18_upload.php
@@ -10,7 +10,7 @@ if(isset($_POST["submit"])){
         echo "File is an image : ".$check["mime"].".";
         $uploadOk=1;
     }else{
-        echo "File is not an image";
+        echo "File is not an image.";
         $uploadOk=0;
     }
 }
